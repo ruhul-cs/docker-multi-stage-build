1b7547108c984a78d92196c3982bd830953b0b43
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-05-10 11:11:40 +0600

    added a jest test script

---
+++ app.test.js
@@ -0,0 +1,11 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+  it('should return Hello, Docker!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, Docker!');
+  });
+});
--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const { createServer } = require('node:http');
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 const HOST_IP_ADDRESS = process.env.HOST_IP_ADDRESS || 'localhost';
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
-
-server.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST_IP_ADDRESS}:${PORT}/`);
+const app = require('./app');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
