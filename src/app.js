676af40c3b1f7e4d3e8c118b733ac65389a3d64b
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-05-13 22:12:32 +0600

    product controller added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const { add } = require("./utils");
-const product = require("../src/controller/product/product")
+const {product} = require("../src/controller/product/product")
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  console.log(product())
-  res.send("Welcome to My App!");
-});
+app.get("/", product);
 
 app.get("/add", (req, res) => {
   const result = add(2, 3);
--- src/controller/product/product.js
+++ src/controller/product/product.js
@@ -1,8 +1,7 @@
 
-const product = ()=>{
-    return [
-        "car","dish","eggs"
-    ]
+const product = async(req,res)=>{
+    const list_item = ["cooker","washingmaching","dish","car","fridge","ac"]
+    return await res.status(200).json(list_item)
 }
 
 module.exports = {
