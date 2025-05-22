68315dc92bc2d5b913ad261e145909a4a02ee205
Atikur Rahman <105223286+iAtikurRahman@users.noreply.github.com>
2025-05-22 12:28:29 +0600

    productV2 func added

--- src/controller/product/product.js
+++ src/controller/product/product.js
@@ -3,3 +3,10 @@ export const product = async (req, res) => {
   const list_item = ['cooker', 'washingmachine', 'dish', 'car', 'fridge', 'ac'];
   return res.json(list_item);
 };
+
+export const productV2 = async (req, res) => {
+  const list_item = ['cooker', 'washingmachine', 'dish', 'car', 'fridge', 'ac'];
+  return res.json(list_item);
+};
+
+
