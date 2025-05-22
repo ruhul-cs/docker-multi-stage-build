964712f94f4feb741ee9148de2ac4ac582021bc4
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-05-22 07:58:45 +0600

    Remove index.js file from root

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello from Docker!'));
-
-app.listen(5000, () => console.log('Server running on port 5000'));
-
