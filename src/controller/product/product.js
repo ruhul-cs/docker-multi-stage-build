af59271746ad3a108a17be2ea6ad75d26875701d
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-05-22 17:01:51 +0600

    Add eslint and fix in product controller

--- src/controller/product/product.js
+++ src/controller/product/product.js
@@ -3,20 +3,15 @@ export const product = async (req, res) => {
   const list_item = ['cooker', 'washingmachine', 'dish', 'car', 'fridge', 'ac'];
   return res.json(list_item);
 };
-
 export const plastic_item = async (req, res) => {
   const list_item = ['bottle', 'jug', 'mog', 'plate', 'glass', 'plastic spoon'];
   return res.json(list_item);
 };
-
 export const kitchen_items = async (req, res) => {
   const list_item = ['cooker', 'washingmachine', 'dish', 'car', 'fridge', 'ac'];
   return res.json(list_item);
 };
-
 export const study_items = async (req, res) => {
   const list_item = ['books', 'chair', 'tables', 'pen', 'pencil', 'scale'];
   return res.json(list_item);
 };
-
-
