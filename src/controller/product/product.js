1f7d7fc16297ca08e7e5a7960a04dafc5e367104
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-05-22 18:31:31 +0600

    Add ride items in product controller

--- src/controller/product/product.js
+++ src/controller/product/product.js
@@ -15,8 +15,11 @@ export const study_items = async (req, res) => {
   const list_item = ['books', 'chair', 'tables', 'pen', 'pencil', 'scale'];
   return res.json(list_item);
 };
-
 export const play_items = async (req, res) => {
   const list_item = ['bat', 'balls', 'stamp', 'footballs', 'hokey', 'sport bike'];
+  return res.json(list_item);
+};
+export const ride_items = async (req, res) => {
+  const list_item = ['car', 'bus', 'truck', 'lorry', 'pickup', 'van'];
   return res.json(list_item);
 };
