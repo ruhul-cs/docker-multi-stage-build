0b91246eb4f84850a7b298de8567e5d16626d46c
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-05-22 07:54:01 +0600

    Apply eslint in the project director
    Use ES format and modulejs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,39 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    rules: {
-      "no-var": "error",
-      "no-multiple-empty-lines": ["error", { max: 0, maxEOF: 0 }],
-      "indent": ["error", 2],
-      "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-    },
-  },
-  {
-    files: ["**/*.js"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
-      sourceType: "commonjs",
-    },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
+      parser: tseslint.parser,
       globals: globals.browser,
+    },
+    plugins: {
+      js
+    },
+    rules: {
+      // Disallow var
+      'no-var': 'error',
+      // Disallow unused variables
+      'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+      // Disallow multiple empty lines
+      'no-multiple-empty-lines': ['error', { max: 0, maxEOF: 0 }],
+      // Enforce 2-space indentation
+      indent: ['error', 2],
+      // Require semicolons
+      semi: ['error', 'always'],
+      // Enforce single quotes
+      quotes: ['error', 'single'],
+      // Enforce "use strict" directive
+      strict: ['error', 'global'],
+    },
+  },
+  // Use CommonJS for *.js files
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
     },
   },
 ]);
